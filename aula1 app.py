f6a6c480a2be7dbd0a8eb1e27fb0af6110e6a436
Vinicius Saviello <viniciussaviello@hotmail.com>
2022-08-17 19:55:32 -0300

    Criando nova rota /teste

--- aula1 app.py
+++ aula1 app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 app = Flask("projeto")
+
+#Rota primaria
 @app.route("/")
 def ola_mundo():   
     nome = "Vinicius Saviello"
@@ -7,5 +9,12 @@ def ola_mundo():
     {"nome": "Console Atual", "preco": 4499.00}]
 
     return render_template("alo.html", n=nome, aProdutos=produtos), 200
+#Rota nova
+@app.route("/teste")
+@app.route("/teste/<variavel>")
+def funcao_teste(variavel=""):
+    return "Nova rota teste<br>Variavel: {}".format(variavel), 200 
+
+
 
 app.run()
