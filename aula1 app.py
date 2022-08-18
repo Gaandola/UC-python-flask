2626773c19c3a0049d0586c437f3879716d01f47
Vinicius Saviello <viniciussaviello@hotmail.com>
2022-08-17 21:42:58 -0300

    Form de login e tratamento de login

--- aula1 app.py
+++ aula1 app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,session,redirect,url_for
 app = Flask("projeto")
+app.secret_key= "WEQUODSAJOISF1239D0A909s0akif" #criar uma chave de criptografia
+
 
 #Rota primaria
 @app.route("/")
@@ -29,5 +31,27 @@ def form_recebe():
         return "Nome: {}".format(nome),200
     else:
         return "Não chamar direto no GET", 200
-    
+
+
+
+@app.route("/login") #Rota do form de login
+def login():
+    return render_template("login.html"),200
+
+@app.route("/login_validar",methods=["POST"]) #rota para validar login/senha
+def login_validar():
+    if request.form["usuario"] == "vinicius" and request.form["senha"] == "123":
+        session["usuario"]= request.form["usuario"]
+        session["codigo"] = 1
+        return redirect(url_for("acesso_restrito"))
+
+    else:
+        return "Login/senha inválidos, digite novamente", 200
+
+@app.route("/restrito") #rota para area restrita
+def acesso_restrito():
+    if session["codigo"] == 1:
+        return "Bem vindo à area restrita <br> Usuário :{} <br> Código: {}".format(session["usuario"], session["codigo"]), 200
+    else: 
+        return "Acesso Inválido" , 200
 app.run()
